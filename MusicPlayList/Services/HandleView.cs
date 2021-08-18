ef49576b0b3f17038e5fd1e1bca7b511683fca6c
VictorSardellaro <victor.henryk@outlook.com>
2021-08-18 19:19:22 -0300

    Update HandleView.cs

--- MusicPlayList/Services/HandleView.cs
+++ MusicPlayList/Services/HandleView.cs
@@ -12,11 +12,11 @@ namespace MusicPlayList.Services
         public static ListViewItem AddListView(string tb_PlaylistTitle, string tb_Singer, string tb_NSong, string tb_Genre, string duration)
         {
 
-            ListViewItem listViewItem = new ListViewItem(tb_PlaylistTitle);
+            ListViewItem listViewItem = new ListViewItem();
             listViewItem.SubItems.Add(tb_PlaylistTitle);
             listViewItem.SubItems.Add(tb_Singer);
-            listViewItem.SubItems.Add(tb_NSong);
             listViewItem.SubItems.Add(tb_Genre);
+            listViewItem.SubItems.Add(tb_NSong);            
             listViewItem.SubItems.Add(duration);
 
             return listViewItem;
